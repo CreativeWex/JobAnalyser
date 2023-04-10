f700d0764932bb2528d9fe765498497e12dbbca3
Wex <bernikcooldude@yandex.ru>
2023-04-10 23:34:26 +0300

    flyway migrations

---
+++ src/main/resources/db/migration/V1__create_clients_table.sql
@@ -0,0 +1,9 @@
+drop table if exists clients cascade;
+
+create table clients (
+                         id bigserial not null,
+                         email varchar(255) not null,
+                         first_name varchar(50) not null,
+                         last_name varchar(255) not null,
+                         primary key (id)
+);
---
+++ src/main/resources/db/migration/V2__init_clients_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO clients (email, first_name, last_name)
+VALUES ('bereznev@gmail.com', 'Nikita', 'Bereznev'),
+       ('petrov@gmail.com', 'Ivan', 'Petrov'),
+       ('smith@gmail.com', 'Ann', 'Smith');
