d6410cf087100bca4d722d1565dfefe417632945
Wex <bernikcooldude@yandex.ru>
2023-05-25 12:32:09 +0300

    redis template configuration

--- src/main/java/com/bereznev/clients/exception/AlreadyExistsException.java
+++ src/main/java/com/bereznev/clients/exception/AlreadyExistsException.java
@@ -8,6 +8,7 @@ package com.bereznev.clients.exception;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
